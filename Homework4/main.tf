09c4b9109733ad49f1f5cad1f2fcfc1739240ae8
andrei marotta <andreiv.marotta@gmail.com>
2024-07-15 17:26:58 +0000

    HW4 update

--- Homework4/main.tf
+++ Homework4/main.tf
@@ -12,10 +12,9 @@ resource "aws_instance" "Name" {
   instance_type = var.instance_type
   count = var.instace_count
   key_name = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = var.open_ports
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
   availability_zone = element(["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"], count.index)
-  associate_public_ip_address = true
-
+    
   tags = {
     Name = "web-${count.index + 1}"
 }
