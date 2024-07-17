7c947088171a46c5910411727939d70a4965be45
andrei marotta <andreiv.marotta@gmail.com>
2024-07-17 04:38:05 +0000

    last update

--- Homework5/ubuntu_ec2.tf
+++ Homework5/ubuntu_ec2.tf
@@ -10,18 +10,17 @@ data "aws_ami" "ubuntu" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "hw5-ubuntu" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = var.ec2_type[1].instance_type
-  subnet_id = aws_subnet.subnet1.id
+  instance_type = var.ec2_type[0].instance_type
+  subnet_id = aws_subnet.public1.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   user_data = file("apache.sh")
 }
 
 output ec2 {
-    value = aws_instance.web.public_ip
+    value = aws_instance.hw5-ubuntu.public_ip
 }
